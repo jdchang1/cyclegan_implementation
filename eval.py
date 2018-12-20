6664d8e3592a9aa6e84f0c2a6fdcc4d996f250fa
Jonathan Chang <jchang@Jonathans-MacBook-Pro-3.local>
2018-12-20 22:04:17 +0900

    tweaks

--- eval.py
+++ eval.py
@@ -5,15 +5,16 @@ from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 from utils import *
 from generator import ResNetGenerator
+from PIL import Image
 
 def eval(data_root, weights_1, weights_2):
     G_A2B = ResNetGenerator(3,3)
     G_B2A = ResNetGenerator(3,3)
-    G_A2B.load_state_dict(torch.load(weights_1))
-    G_B2A.load_state_dict(torch.load(weights_2))
+    G_A2B.load_state_dict(torch.load(weights_1, map_location={'cuda:0': 'cpu'}))
+    G_B2A.load_state_dict(torch.load(weights_2, map_location={'cuda:0': 'cpu'}))
     G_A2B.eval()
     G_B2A.eval()
-    tsfms = [transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
+    tsfms = [transforms.Resize(256, Image.BICUBIC), transforms.RandomCrop(256), transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
     dataset = ImageDataset(data_root, tsfms=tsfms, mode='test')
     dataloader = DataLoader(dataset, batch_size=1, num_workers=4)
     input_A = torch.zeros([1,3,256,256]).type(torch.FloatTensor)
@@ -23,7 +24,7 @@ def eval(data_root, weights_1, weights_2):
         real_B = input_B.copy_(batch['B'])
         fake_A = 0.5*(G_B2A(real_B).data + 1.0)
         fake_B = 0.5*(G_A2B(real_A).data + 1.0)
-        save_image(fake_A, 'output/summer2winter/testA/{}.png'.format(i+1))
-        save_image(fake_B, 'output/summer2winter/testB/{}.png'.format(i+1))
+        save_image(fake_A, 'output/fort2cod/source_B/{}.png'.format(i+1))
+        save_image(fake_B, 'output/fort2cod/source_A/{}.png'.format(i+1))
 
 eval(sys.argv[1], sys.argv[2], sys.argv[3])
